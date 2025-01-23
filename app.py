9c23fefc0fa32cab2dee7bf0585487930d227b07
sidiq20 <sidiqolasode@gmail.com>
2025-01-23 21:10:20 +0100

    dates added and the  templates are chnaged

--- app.py
+++ app.py
@@ -1,17 +1,40 @@
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 habits = ["Test habit", "Test habit 2"]
 
+
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range (-3, 4)]
+        return dates
+
+    return {"date_range": date_range}
+
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+
+    return render_template(
+        "index.html",
+        habits=habits,
+        title="Habit Tracker - Home",
+        selected_date=selected_date,
+    )
 
 @app.route("/add", methods=["GET", "POST"])
 def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template("add_habit.html",
+                           title="Habit Tracker - Add Habit",
+                           selected_date=datetime.date.today(),)
 
 
 if __name__ == "__main__":
