3e96f4e957c46459bf7cad074a64e86bbda6d4c5
sidiq20 <sidiqolasode@gmail.com>
2025-01-24 21:50:41 +0100

    added routes

--- app.py
+++ app.py
@@ -1,41 +1,11 @@
 from flask import Flask
 from routes import pages
 
-app = Flask(__name__)
-habits = ["Test habit", "Test habit 2"]
-
-
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        dates = [start + datetime.timedelta(days=diff) for diff in range (-3, 4)]
-        return dates
-
-    return {"date_range": date_range}
-
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
-
-    return render_template(
-        "index.html",
-        habits=habits,
-        title="Habit Tracker - Home",
-        selected_date=selected_date,
-    )
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habits.append(request.form.get("habit"))
-    return render_template("add_habit.html",
-                           title="Habit Tracker - Add Habit",
-                           selected_date=datetime.date.today(),)
-
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(pages)
+    
+    return app
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
