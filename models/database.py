0edecaeddd03b7d2e6c24c82ded8e0f52c4ba06d
sidiq20 <sidiqolasode@gmail.com>
2025-01-28 12:01:44 +0100

    adding the models

--- models/database.py
+++ models/database.py
@@ -23,3 +23,51 @@ class Database:
                 'timezone': timezone,
                 'create': datetime.utcnow()
             }
+            result = self.db.users.insert_one(user)
+            user['_id'] = result.inserted_id
+            return user
+        except Exception as e:
+            print(f"error creating the user: {e}")
+            return None
+
+        def get_user_by_email(self, email: str) -> Optional[dict]:
+            return self.db.users.find_one({'email': email.lower()})
+
+        def verify_password(self, user: dict, password: str) -> bool:
+            return check_password_hash(user['password'], password)
+
+        def create_habit(self, user_id: str, name: str, reminder_time: str = '19:00') -> Optional[dict]
+            try:
+                habit = {
+                    'userid': ObjectId(user_id),
+                    'name': name.strip(),
+                    'reminderTime': reminder_time,
+                    'createAt': datetime.utcnow(),
+                    'lastReminderSent': None
+                }
+                result = self.db.habits.insert_one(habit)
+                habit['_id'] = result.inserted_id
+                return habit
+            except Exception as e:
+                print(f"error creating habit: {e}")
+                return None
+
+        def get_user_habits(self, user_id: str) -> list:
+            return list(self.db.habits.find({'userId': ObjectId(user_id)}))
+
+        def complete_habit(self, habit_id: str, date: datetime) -> bool:
+            try:
+                self.db.completions.insert_one({
+                    'habitId': ObjectId(habit_id),
+                    'date': date,
+                    'createAt': datetime.utcnow()
+                })
+                return True
+            except Exception as e:
+                print(f"Error completing habit: {e}")
+                return False
+
+        def get_completion(self, habit_id: str, date: datetime) -> Optional[dict]:
+            return self.db.completions.find_one({
+                'habit'
+            })
